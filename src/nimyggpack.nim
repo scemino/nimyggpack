98e3b03859501a8163b88ee550c0f2d8f44b387b
scemino <scemino74@gmail.com>
2022-09-07 20:43:20 +0200

    Add savegame

--- src/nimyggpack.nim
+++ src/nimyggpack.nim
@@ -1,4 +1,4 @@
-import std/[streams, tables, strformat]
+import std/[streams, tables, strformat, times]
 import glob
 import nimyggpackpkg/ggtable_decoder, nimyggpackpkg/range_stream,
     nimyggpackpkg/xor_stream, nimyggpackpkg/bnut_decoder, nimyggpackpkg/ggpack_decoder,
@@ -13,7 +13,7 @@ export newXorDecodeStream, newXorEncodeStream, xorKeys, xorDecode, xorEncode
 export bnutEncode, bnutDecode
 export ggtableEncode, ggtableDecode
 export Savegame
-export loadSaveGame
+export loadSaveGame, saveSaveGame
 
 when isMainModule:
   import std/[parseopt, strutils, os, json]
@@ -31,12 +31,14 @@ when isMainModule:
                                   Possible names: 56ad, 5bad, 566d, 5b6d, delores
     --list=pattern      -l        Lists files in a ggpack file
     --extract=pattern   -x        Extracts files from a ggpack file
+    --savetojson=file   -s        Extracts savegame to json
+    --savefromjson=file -j        Creates savegame from json
     --noconvert                   Disables auto conversion: .bnut to .nut, .wimpy to json, .byack to .yack
   """
 
   type
     Command = enum
-      cmdList, cmdExtract, cmdUpdate
+      cmdList, cmdExtract, cmdUpdate, cmdSaveToJson, cmdSaveFromJson
     Settings = object
       filename, pattern: string
       xorKey: string
@@ -84,6 +86,20 @@ when isMainModule:
         else:
           convert(decoder, k)
 
+  proc doSaveToJson(settings: Settings) =
+    var fn = settings.pattern
+    let savegame = loadSaveGame(fn)
+    fn = changeFileExt(fn, ".json")
+    writeFile(fn , pretty(savegame.data))
+
+  proc doSaveFromJson(settings: Settings) =
+    var fn = settings.pattern
+    let json = parseFile(fn)
+    let savetime = fromUnix(json["savetime"].getInt)
+    let savegame = Savegame(data: json, time: savetime)
+    fn = changeFileExt(fn, ".save")
+    saveSaveGame(fn, savegame)
+
   proc doUpdate(settings: Settings) =
     echo fmt"update({settings.pattern}, {settings.filename}, {settings.xorKey})"
     let backupFilename = settings.filename & ".bak"
@@ -107,6 +123,13 @@ when isMainModule:
     echo Usage
     quit(0)
       
+  proc checkFilename(settings: Settings): bool =
+    if settings.filename == "":
+      writeHelp()
+      result = false
+    else:
+      result = true
+
   var settings = Settings(xorKey: "56ad")
   var p = initOptParser()
   # parse options
@@ -128,16 +151,28 @@ when isMainModule:
       of "u", "update":
         settings.cmd = cmdUpdate
         settings.pattern = val
+      of "s", "savetojson":
+        settings.cmd = cmdSaveToJson
+        settings.pattern = val
+      of "j", "savefromjson":
+        settings.cmd = cmdSaveFromJson
+        settings.pattern = val
       of "noconvert":
         settings.noconvert = true
     of cmdArgument:
       settings.filename = key
-  if settings.filename == "":
-    writeHelp()
+  
   case settings.cmd:
   of cmdList:
-    doList(settings)
+    if checkFilename(settings):
+      doList(settings)
   of cmdExtract:
-    doExtract(settings)
+    if checkFilename(settings):
+      doExtract(settings)
   of cmdUpdate:
-    doUpdate(settings)
+    if checkFilename(settings):
+      doUpdate(settings)
+  of cmdSaveToJson:
+    doSaveToJson(settings)
+  of cmdSaveFromJson:
+    doSaveFromJson(settings)
--- src/nimyggpackpkg/savegame.nim
+++ src/nimyggpackpkg/savegame.nim
@@ -3,24 +3,29 @@ import std/json
 import std/algorithm
 import std/sets
 import ggtable_decoder
+import ggtable_encoder
+
+proc memset*(p: pointer, value: cint, size: csize_t) {.importc, header: "<string.h>".}
 
 const DELTA = 0x9e3779b9'u32
 
-type Savegame* = object
-  data*: JsonNode
-  time*: Time
+type
+  Savegame* = object
+    data*: JsonNode
+    time*: Time
+  HashError* = object of CatchableError
 
-var savegameKey = [0xAEA4EDF3'u32, 0xAFF8332A'u32, 0xB5A2DBB4'u32, 0x9B4BA022'u32]
+const savegameKey = [0xAEA4EDF3'u32, 0xAFF8332A'u32, 0xB5A2DBB4'u32, 0x9B4BA022'u32]
 
 proc mx(sum, y, z: uint32, p, e: uint, k: openArray[uint32]): uint32 =
-    result = (z shr 5 xor y shl 2) + (y shr 3 xor z shl 4) xor (sum.uint32 xor y) + (k[p and 3 xor e] xor z)
+    result = ((z shr 5 xor y shl 2) + (y shr 3 xor z shl 4)) xor ((sum xor y) + (k[(p and 3) xor e] xor z))
 
 proc decrypt(v: var openArray[uint32], key: openArray[uint32]) =
-  var sum, y, z, rounds: uint32
-  var p, e: uint
+  var sum, y, z: uint32
+  var p, rounds, e: uint32
   let n = v.len
   rounds = (6 + 52 div n).uint32
-  sum = (rounds * DELTA)
+  sum = (rounds * DELTA).uint32
   y = v[0]
   for i in 0..<rounds:
     e = (sum shr 2) and 3
@@ -33,27 +38,65 @@ proc decrypt(v: var openArray[uint32], key: openArray[uint32]) =
     y = v[0]
     sum -= DELTA
 
-proc computeHash(data: openArray[byte]): int32 =
+proc encrypt(v: var openArray[uint32], key: openArray[uint32]) =
+  var sum, y, z: uint32
+  var p, rounds, e: uint32
+  let n = v.len.uint32
+  if n > 1:
+    rounds = (6 + 52 div n).uint32
+    sum = 0
+    z = v[n - 1]
+    for i in 0..<rounds:
+      sum += DELTA
+      e = (sum shr 2) and 3
+      for p in 0'u..<(n - 1).uint32:
+        y = v[p + 1]
+        v[p] += mx(sum, y, z, p, e, key)
+        z = v[p]
+      p = n - 1
+      y = v[0]
+      v[n - 1] += mx(sum, y, z, p, e, key)
+      z = v[n - 1]
+
+proc computeHash(data: string): int32 =
   result = 0x6583463'i32
   for i in 0..<data.len:
     result += data[i].int32
-
-template toOpenArr[Tin, Tout](arr: openArray[Tin], first, size: int, t: Tout): openArray[Tout] =
-  var p = cast[ptr UncheckedArray[Tout]](arr[0].addr)
-  p.toOpenArray(first, (first + size) * sizeof(Tin) div sizeof(Tout) - 1)
 
 template toOpenArr[Tin, Tout](arr: openArray[Tin], t: Tout): openArray[Tout] =
   var p = cast[ptr UncheckedArray[Tout]](arr[0].addr)
   p.toOpenArray(0, arr.len * sizeof(Tin) div sizeof(Tout) - 1)
 
 proc loadSaveGame*(path: string): Savegame =
-  var f = open(path, fmRead)
-  var data = f.readAll()
+  var data = readFile(path)
   decrypt(data.toOpenArr(0'u32), savegameKey)
 
-  let hashData = cast[ptr int32](data[data.len-16].addr)[]
-  let t = fromUnix(cast[ptr int32](data[data.len-12].addr)[])
+  let hashData = cast[ptr int32](data[^16].addr)[]
+  let t = fromUnix(cast[ptr int32](data[^12].addr)[])
   
-  let hashCheck = computeHash(data.toOpenArr(0, data.len-16, 0'u8))
+  let hashCheck = computeHash(data[0..^17])
   if hashData == hashCheck:
     result = Savegame(data: ggtableDecode(data), time: t)
+  else:
+    raise newException(HashError, "Invalid savegame")
+
+proc saveSaveGame*(path: string, savegame: Savegame) =
+  var data = ggtableEncode(savegame.data)
+  let hash = computeHash(data)
+
+  let fullSizeAndFooter = data.len + 16
+  let marker = (8 - ((data.len + 9) mod 8)).cint
+  data.setLen fullSizeAndFooter
+
+  # write at the end 16 bytes: hashdata (4 bytes) + savetime (4 bytes) + marker (8 bytes)
+  var p = cast[ptr int32](data[^16].addr)
+  p[] = hash
+  p = cast[ptr int32](data[^12].addr)
+  p[] = savegame.time.toUnix.int32
+  memset(data[^8].addr, marker, 8)
+
+  # then encode data
+  encrypt(data.toOpenArr(0'u32), savegameKey)
+
+  # and write data
+  writeFile(path, data)
